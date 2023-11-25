9b7b15483174154d09d28d8cd61ae5df350864c6
Seweryn <sew_k@wp.pl>
2023-11-25 12:46:01 +0100

    Added ViewConfig class for code cleaning purposes.

---
+++ src/main/java/com/kodilla/sudokujavafx/presentation/ViewConfig.java
@@ -0,0 +1,7 @@
+package com.kodilla.sudokujavafx.presentation;
+
+public class ViewConfig {
+    public static String FIXED_SUDOKU_ELEMENT = "-fx-text-fill: BLACK; -fx-border-color: gray; -fx-font-size: 18; -fx-font-weight: bold";
+    public static String NOT_FIXED_CORRECT_SUDOKU_ELEMENT = "-fx-text-fill: BLUE; -fx-border-color: gray; -fx-font-size: 18";
+    public static String NOT_FIXED_INCORRECT_SUDOKU_ELEMENT = "-fx-text-fill: RED; -fx-border-color: gray; -fx-font-size: 18";
+}
